2c8c38ccc0267e6d772bb4871d8337f7f15cf48f
dotshef <dotshef@gmail.com>
2026-07-31 18:01:39 +0900

    feat: 부정 클릭 방지 스크립트 삽입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,10 @@ export default function RootLayout({
 	var _AceGID=(function(){var Inf=['theareport.com','www.theareport.com,theareport.com','AZ3A106053','1','NaPm,Ncisy','1']; var _CI=(!_AceGID)?[]:_AceGID.val;var _N=0;if(_CI.join('.').indexOf(Inf[2])<0){ _CI.push(Inf);  _N=_CI.length; } return {o: _N,val:_CI}; })();
 	var _AceCounter=(function(){var G=_AceGID;var _sc=document.createElement('script');var _sm=document.getElementsByTagName('script')[0];if(G.o!=0){var _A=G.val[G.o-1];var _U=(_A[4]).replace(/\\,/g,'_');_sc.src='https:/'+'/cr.acecounter.com/ac.j'+'s?gc='+_A[2]+'&py='+_A[1]+'&up='+_U+'&rd='+(new Date().getTime());_sm.parentNode.insertBefore(_sc,_sm);return _sc.src;}})();
         `}</Script>
+        {/* BS Report Log Script */}
+        <Script id="bslog" strategy="afterInteractive">{`
+	(function(){var JsHost=(("https:"==document.location.protocol)?"https://":"http://");var sTime=new Date().getTime();var _sc=document.createElement('script');_sc.id='bslog_script';_sc.type='text/javascript';_sc.async=true;_sc.src=JsHost+'bs-report.lob.kr/ntrace.js?bs_id=bsgroup&bs_m=4456985&t='+sTime;var _sm=document.getElementsByTagName('script')[0];_sm.parentNode.insertBefore(_sc,_sm);})();
+        `}</Script>
       </body>
     </html>
   );
